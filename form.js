a0248c4c73c38d38b2c0e421298788f9501bcc15
firodebele <130435394+firodebele@users.noreply.github.com>
2023-07-01 02:01:34 -0700

    Add files via upload

---
+++ form.js
@@ -0,0 +1,25 @@
+// // Access the form elements
+// const usernameInput = document.getElementById("username");
+// const passwordInput = document.getElementById("password");
+// const loginForm = document.querySelector("form");
+
+// // Add event listener for form submission
+// loginForm.addEventListener("submit", function(event) {
+//   event.preventDefault(); // Prevent form submission
+
+//   // Get the entered username and password
+//   const username = usernameInput.value;
+//   const password = passwordInput.value;
+
+//   // Perform login validation or further processing here
+//   // Example:
+//   if (username === "admin" && password === "password") {
+//     alert("Login successful!");
+//   } else {
+//     alert("Invalid username or password. Please try again.");
+//   }
+
+//   // Clear the form inputs
+//   usernameInput.value = "";
+//   passwordInput.value = "";
+// });
